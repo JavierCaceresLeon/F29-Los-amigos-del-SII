8fc73eaf6df2a2a7fae71269f8cbaeb39e299775
vicentetejos1 <vicente.tejos@usm.cl>
2024-05-06 19:00:39 -0400

    login

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+// Path: src/index.js
+require('express')
+
+app = express()
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    }
+)
+// login access
+
+app.post(/login/, (req, res) => {
+    res.send('User logged in successfully')
+    let username=req.body.username;
+    let password=req.body.password;
+    res.send(`Usernme: ${username} Pass: ${password}`)
+    }
+)
+// object array para usuario
+
+var ObjectArray1 = [{username:"user00",password:"password",email:"example.com",rut:"12345678-9"}]
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+    }
+)
